36bdeb4cfe5e44e0536ad453fc8b6ec414261631
felicitaspineirob <162723113+felicitaspineirob@users.noreply.github.com>
2024-05-23 18:49:34 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,12 +1,22 @@
 def index_of_by_index(word, list, index):
+    i=0
+    while i < len(my_list):
+        if my_list(i)==word:
+            return i
+        i+=1
     return -1
-
-
+    
 def index_of_empty(list):
+    i=0
+    while i < len(my_list):
+        if my_list[i]=="":
+            my_list[i]= word
+            return i
+        i+=1
     return -1
-
 
 def index_of(word, list):
+    
     return -1
 
 
