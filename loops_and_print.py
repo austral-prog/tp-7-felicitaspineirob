5e5c4f40b334b167ab27064f16dc90584988c125
felicitaspineirob <162723113+felicitaspineirob@users.noreply.github.com>
2024-05-23 21:21:22 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,22 @@
 def enumerate_list(list):
-    return list
 
+    lists = []
+    lists2 = []
+    for e in list:
+        if e != "":
+            lists.append(e)
+    for i, e in enumerate(lists[0:]):
+        lists2.append(f"{i}. {e}")
+    return lists2
 
+#retorna la lista:
+# ["0. Red", "1. Green", "2. White", "3. Black"]
 def enumerate_backwards(list):
-    return list
+    lists = []
+    lists2 = []
+    for e in list:
+        if e != "":
+            lists.append(e[::-1])
+    for i, e in enumerate(lists[0:]):
+        lists2.append(f"{i}. {e}")
+    return lists2
